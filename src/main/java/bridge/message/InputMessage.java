85eb106d2da6244d03a99b44860076e052f3031f
BOYOUNG-KANG <28287@naver.com>
2023-08-30 16:20:20 +0900

    feat(InputMessage) : 입력 메시지 클래스 생성

---
+++ src/main/java/bridge/message/InputMessage.java
@@ -0,0 +1,8 @@
+package bridge.message;
+
+public class InputMessage {
+    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    public static final String BRIDGE_LENGTH_INPUT_MESSAGE = "다리의 길이를 입력해주세요.";
+    public static final String PLAYER_MOVEMENT_INPUT_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    public static final String RESTART_YN_INPUT_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+}
