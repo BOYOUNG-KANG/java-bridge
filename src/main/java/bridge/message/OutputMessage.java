7269eff2e96df0d83e7986639c52bcdfd7641bac
BOYOUNG-KANG <28287@naver.com>
2023-08-30 16:36:07 +0900

    feat(OutputMessage) : 출력 메시지 클래스 생성 및 시작 알림 변경

---
+++ src/main/java/bridge/message/OutputMessage.java
@@ -0,0 +1,5 @@
+package bridge.message;
+
+public class OutputMessage {
+    public static final String GAME_START_MESSAGE = "다리 건너기 게임을 시작합니다.";
+}
---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,34 @@
+package bridge.view;
+
+import bridge.message.InputMessage;
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public int readBridgeSize() {
+        System.out.println(InputMessage.BRIDGE_LENGTH_INPUT_MESSAGE);
+        return Integer.parseInt(Console.readLine());
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public String readMoving() {
+        System.out.println(InputMessage.PLAYER_MOVEMENT_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public String readGameCommand() {
+        System.out.println(InputMessage.RESTART_YN_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+}
