b344334269f147e5a0ca092d5f937a9d75927115
BOYOUNG-KANG <28287@naver.com>
2023-08-30 16:00:12 +0900

    feat(InputErrorMessage) : 입력값 에러 메시지 클래스 생성

---
+++ src/main/java/bridge/message/InputErrorMessage.java
@@ -0,0 +1,7 @@
+package bridge.message;
+
+public class InputErrorMessage {
+    public static final String BRIDGE_LENGTH_INPUT_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String PLAYER_MOVEMENT_INPUT_ERROR = "[ERROR] 이동할 칸은 U나 D여야 합니다.";
+    public static final String RESTART_YN_INPUT_ERROR = "[ERROR] 게임 재시작 여부는 R이나 Q여야 합니다.";
+}
