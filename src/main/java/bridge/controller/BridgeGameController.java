95e753d40169e273a1af8916a5a68a93ea903cc2
BOYOUNG-KANG <28287@naver.com>
2023-08-31 18:20:43 +0900

    feat(BridgeMaker) : 다리 길이 입력 및 유효성 검사 기능 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,16 +1,20 @@
 package bridge.controller;
 
+import bridge.BridgeRandomNumberGenerator;
+import bridge.domain.BridgeMaker;
+import bridge.domain.PlayerMovement;
+import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeGameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
-    
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     PlayerMovement playerMovement = new PlayerMovement();
     public void startGame(){
         outputView.startGame();
         int bridgeSize = inputView.readBridgeSize();
         playerMovement.addPlayerMove(inputView.readMoving());
-
+        bridgeMaker.makeBridge(bridgeSize);
     }
 }
