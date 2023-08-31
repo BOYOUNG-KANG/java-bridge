76a7fa9a364495ed8c37fc1d2c4a4fbd48e8ec9b
BOYOUNG-KANG <28287@naver.com>
2023-08-31 17:01:31 +0900

    feat(BridgeGameController) : 다리 길이 받는 로직 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -4,7 +4,9 @@ import bridge.view.OutputView;
 
 public class BridgeGameController {
     OutputView outputView = new OutputView();
+    InputView inputView = new InputView();
     public void startGame(){
         outputView.startGame();
+        int bridgeSize = inputView.readBridgeSize();
     }
 }
