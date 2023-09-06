9a98402667f080833b6fe186ac6fee5442126be3
BOYOUNG-KANG <28287@naver.com>
2023-09-06 16:33:20 +0900

    feat(BridgeGameController) : 현재 다리 이미지 출력 로직 추가

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -12,22 +12,36 @@ import java.util.List;
 public class BridgeGameController {
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
-
     public void startGame(){
         //initSetup
         PlayerMovement playerMovement = new PlayerMovement();
-        BridgeMap bridgeMap = new BridgeMap();
+        String failYn = "N";
 
         outputView.startGame();
         int bridgeSize = inputView.readBridgeSize();
+        System.out.println();
         BridgeRandomNumberGenerator generator = new BridgeRandomNumberGenerator();
         BridgeMaker bridgeMaker = new BridgeMaker(generator);
         List<String> bridgeBlock = bridgeMaker.makeBridge(bridgeSize);
 
-        playGame(bridgeSize, bridgeBlock, playerMovement, bridgeMap);
+        for(int i = 1; i <= bridgeSize; i ++) {
+            if(failYn.equals("N")) {
+                failYn = playGame(i, bridgeBlock, playerMovement);
+            }
+        }
+
+
+
     }
-    public void playGame(int size, List<String> block, PlayerMovement playerMovement, BridgeMap bridgeMap){
+    public String playGame(int size, List<String> block, PlayerMovement playerMovement){
+        System.out.println();
         playerMovement.addPlayerMove(inputView.readMoving());
+        BridgeMap bridgeMap = new BridgeMap();
         bridgeMap.createBridge(size, playerMovement.getPlayerMove(), block);
+        System.out.println("bridgeMap = " + bridgeMap.getUpperBridgeMap());
+        System.out.println("bridgeMap = " + bridgeMap.getLowerBridgeMap());
+
+        outputView.printMap(bridgeMap.getUpperBridgeMap(), bridgeMap.getLowerBridgeMap());
+        return bridgeMap.getFailYn();
     }
 }
